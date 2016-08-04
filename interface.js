daf6a0ace7dbe5dbc82a7305299357e646909192
Michael <mjosephmiller.uk@gmail.com>
2016-08-04 17:19:18 +0100

    thermostart working

---
+++ interface.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
+  displayWeather('London');
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=044e356b671b8e64178e84aad3771111';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+    });
+  }
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#body').attr('class', thermostat.energyUsage());
+  }
+
+  $('#temp-up').on('click', function(){
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temp-down').on('click', function(){
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function(){
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function(){
+    thermostat.powerSavingModeOn();
+    $('#powersaving-status').text('on');
+    updateTemperature();
+  });
+
+  $('#powersaving-off').click(function(){
+    thermostat.powerSavingModeOff();
+    $('#powersaving-status').text('off');
+    updateTemperature();
+  });
+});
